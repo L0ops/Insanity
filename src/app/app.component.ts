7f0dc2d4ad88142e887c6dc292fb29c018a2a7ef
L0ops <cerny_s@etna-alternance.net>
2017-12-02 16:27:24 +0100

    #4 set object animations + path to sprite, play animations move/idle of Player

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -33,11 +33,30 @@ export class AppComponent {
     var light = new BABYLON.PointLight("Point", new BABYLON.Vector3(5, 10, 5), scene);
     var freeCamera = new BABYLON.FreeCamera("FreeCamera", new BABYLON.Vector3(0, 0, -10), scene);
 
-    var player = new Player("../assets/mark.png", scene);
+    var playersPath = { mark : "../assets/mark.png"};
+
+    // exemple of 1 player animations
+    var markAnimation = {
+      idle : {
+        begin : 0,
+        end : 3,
+        speed : 300
+      },
+      move : {
+        begin : 4,
+        end : 7,
+        speed : 300
+      }
+    }
+    // exemple return of get animations from api
+    var animations = { mark : markAnimation };
+
+
+    var player = new Player(playersPath.mark, scene, animations.mark);
     var playerControle = {left:'a', right:'d'};
     var playerKeys = new KeyBind(playerControle, player);
 
-    var player2 = new Player("../assets/mark.png", scene);
+    var player2 = new Player(playersPath.mark, scene, animations.mark);
     var player2Controle = {left:'j', right:'l'};
     var player2Keys = new KeyBind(player2Controle, player2);
 
