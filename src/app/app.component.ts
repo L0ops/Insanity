bce5a8d0d37bdc5f521010f9d25b56096211ef7c
L0ops <cerny_s@etna-alternance.net>
2017-12-07 10:00:28 +0100

    Issue #35 Create Arbitre Instance + refactor code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import Block from '../lib/Block';
 import Player from '../lib/Player';
 import KeyBind from '../lib/KeyBind';
 import Key from '../lib/Key';
-import KeyGenerator from '../lib/KeyGenerator';
+import Arbitre from '../lib/Arbitre';
 
 @Component({
   selector: 'app-root',
@@ -34,14 +34,11 @@ export class AppComponent implements AfterViewInit {
   createScene = function () {
     const scene = new BABYLON.Scene(this.engine);
     scene.actionManager = new BABYLON.ActionManager(scene);
-    const background = new BABYLON.Layer("back", "../assets/Sprites/map.png", scene);
-          background.isBackground = true;
-          background.texture.level = 0;
-          background.texture.wAng = 0;
+
+    this.createBackground("../assets/Sprites/map.png", scene);
 
     const light = new BABYLON.PointLight('Point', new BABYLON.Vector3(5, 10, 5), scene);
     const freeCamera = new BABYLON.FreeCamera('FreeCamera', new BABYLON.Vector3(0, 0, -10), scene);
-    const playersPath = "../assets/Sprites/cosm.png";
 
     const keys_array = [['q','w'],['a','s'],['i', 'o'], ['k','l']];
     const keys = [];
@@ -49,57 +46,55 @@ export class AppComponent implements AfterViewInit {
       var key = new Key(keys_array[i][0], keys_array[i][1]);
       keys.push(key);
     }
-    // exemple of 1 player animations
-    const markAnimation = {
-      idle: {
-        begin: 0,
-        end: 3,
-        speed: 100
-      },
-      move: {
-        begin: 4,
-        end: 7,
-        speed: 300
-      },
-      dash: {
-        begin: 10,
-        end: 15,
-        speed: 50
-      },
-      jump: {
-        begin: 16,
-        end: 21,
-        speed: 150
-      }
-    };
-    // example return of get animations from api
-    const animations = {mark: markAnimation};
-    const spriteManagerPlayer = new BABYLON.SpriteManager("pm", playersPath, 3, 80, scene);
 
     var world = new p2.World({
       gravity: [0, -9.82]
     });
+    Arbitre.getInstance().setScene(scene);
+    Arbitre.getInstance().setWorld(world);
+    const playersName = ['player1', 'player2', 'player3'];
 
-    const player = new Player("player1", scene, animations.mark, spriteManagerPlayer);
-    player.body.position[0] -= 3;
-    world.addBody(player.body);
-    const player2 = new Player("player2", scene, animations.mark, spriteManagerPlayer);
-    player2.body.position[0] += 3;
-    world.addBody(player2.body);
+    for (var i in playersName) {
+      var position = -3;
+      Arbitre.getInstance().createPlayer(playersName[i], position);
+      position += 3;
+    }
 
-    const player3 = new Player("player3", scene, animations.mark, spriteManagerPlayer);
-    player3.body.position[0] += 3;
-    world.addBody(player3.body);
-    const players = [];
-    players.push(player);
-    players.push(player2);
-    players.push(player3);
+    let players = Arbitre.getInstance().getPlayers();
 
-    const block = new Block("block1", scene, spriteManagerPlayer);
-    block.body.position[0] -= 3;
-    block.body.position[1] -= 1;
-    world.addBody(block.body);
+    this.createGround(world, players);
 
+    Arbitre.getInstance().setTimerKeys(10000).setKeys(keys).addPlayersToGenerate().generateKeys();
+    Arbitre.getInstance().regenerate();
+    players = Arbitre.getInstance().getPlayers();
+    this.setCollision(world, players);
+
+    scene.registerBeforeRender(() => {
+      world.step(1/60);
+      for (var i in players) {
+        this.playerAction(players[i]);
+        players[i].update();
+      }
+    });
+    return scene;
+  };
+
+  createBackground = function (path: string, scene: BABYLON.Scene) {
+    const background = new BABYLON.Layer("back", path, scene);
+          background.isBackground = true;
+          background.texture.level = 0;
+          background.texture.wAng = 0;
+  };
+
+  setCollision = function (world: p2.World, players: Player[]) {
+    world.on('beginContact', (evt) => {
+      if (players[evt.bodyA.id - 1] && players[evt.bodyB.id - 1]) {
+        this.collisionDash(evt, players);
+      }
+    });
+  };
+
+  createGround = function(world: p2.World, players: Player[]) {
     const groundBody = new p2.Body({mass: 0});
     const groundPlane = new p2.Plane();
     groundBody.position[1] = -3;
@@ -107,75 +102,50 @@ export class AppComponent implements AfterViewInit {
     groundPlane.material = groundMaterial;
     groundBody.addShape(groundPlane);
     world.addBody(groundBody);
-
     for (var i in players) {
       world.addContactMaterial(new p2.ContactMaterial(groundMaterial, players[i].material, {
         friction: 2.0
       }));
     }
+  };
 
-    for (var i in players) {
-      world.addContactMaterial(new p2.ContactMaterial(block.material, players[i].material, {
-        friction: 2.0
-      }));
+  playerAction = function(player: Player) {
+    let move = true;
+    if (player.isMoving) {
+      let force = player.moveLeft ? -4.5 : 4.5;
+      player.move(force);
+    } else if (player.doDash) {
+      let force = player.dashLeft ? -10 : 10;
+      player.dash(force);
+    } else if (player.hit) {
+      player.takeDash();
+    } else {
+      move = false;
     }
+    if (player.isJumping) {
+      player.jump();
+    }
+    if (!move) {
+      player.move(0);
+    }
+  };
 
-    KeyGenerator.getInstance().addKeys(keys).addPlayers(players).generate();
-
-    setTimeout(function () {
-      KeyGenerator.getInstance().clean();
-      KeyGenerator.getInstance().generate();
-    }, 10000);
-
-    world.on('beginContact', function (evt) {
-      if (players[evt.bodyA.id - 1] && players[evt.bodyB.id - 1]) {
-        let dasher: number;
-        let touched: number;
-        if (players[evt.bodyA.id - 1].doDash || players[evt.bodyB.id - 1].doDash) {
-          dasher = (players[evt.bodyA.id - 1].doDash ? evt.bodyA.id - 1 : evt.bodyB.id - 1);
-          touched = (players[evt.bodyA.id - 1].doDash ? evt.bodyB.id - 1 : evt.bodyA.id - 1);
-        }
-        if (dasher != null && touched != null &&
-          players[dasher].doDash && players[touched].doDash) {
-          const rand = KeyGenerator.getInstance().getRandomInt(0, 2);
-          dasher = (rand == 0 ? evt.bodyA.id - 1 : evt.bodyB.id - 1);
-          touched = (rand == 0 ? evt.bodyB.id - 1 : evt.bodyA.id - 1);
-        }
-        if (dasher != null && touched != null) {
-          console.log(players[dasher].name, "a fait un dash a", players[touched].name);
-          players[touched].hitByDash(players[dasher].dashLeft ? -1 : 1);
-          players[dasher].stopDash();
-        }
+  collisionDash = function (evt: p2.EventEmitter, players: Player[]) {
+    let dasher: number;
+    let touched: number;
+    if (players[evt.bodyA.id - 1].doDash || players[evt.bodyB.id - 1].doDash) {
+      dasher = (players[evt.bodyA.id - 1].doDash ? evt.bodyA.id - 1 : evt.bodyB.id - 1);
+      touched = (players[evt.bodyA.id - 1].doDash ? evt.bodyB.id - 1 : evt.bodyA.id - 1);
+      if (players[dasher].doDash && players[touched].doDash) {
+        Arbitre.getInstance().parityDash(evt.bodyA.id - 1, evt.bodyB.id - 1);
+        dasher = Arbitre.getInstance().getDasher();
+        touched = Arbitre.getInstance().getTouchedByDash();
       }
-    });
-
-    scene.registerBeforeRender(function () {
-      world.step(1/60);
-
-      for (var i in players) {
-        let move = true;
-        if (players[i].isMoving) {
-          let force = players[i].moveLeft ? -4.5 : 4.5;
-          players[i].move(force);
-        } else if (players[i].doDash) {
-          let force = players[i].dashLeft ? -10 : 10;
-          players[i].dash(force);
-        } else if (players[i].hit) {
-          players[i].takeDash();
-        } else {
-          move = false;
-        }
-        if (players[i].isJumping) {
-          players[i].jump();
-        }
-        if (!move) {
-          players[i].move(0);
-        }
-        players[i].update();
-      }
-      block.update();
-
-    });
-    return scene;
+    }
+    if (dasher != null && touched != null) {
+      console.log(players[dasher].name, "a fait un dash a", players[touched].name);
+      players[touched].hitByDash(players[dasher].dashLeft ? -1 : 1);
+      players[dasher].stopDash();
+    }
   };
 }
