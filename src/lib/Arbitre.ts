9837e88979fee7698c7496ec499f6594bbe5c9a0
Tristan Marion <trismarion@gmail.com>
2017-12-08 17:08:28 +0100

    Formating Arbitre.ts

--- src/lib/Arbitre.ts
+++ src/lib/Arbitre.ts
@@ -1,6 +1,6 @@
 import KeyGenerator from '../lib/KeyGenerator';
-import Key from './Key'
-import Player from './Player'
+import Key from './Key';
+import Player from './Player';
 import * as p2 from 'p2';
 import * as BABYLON from 'babylonjs';
 
@@ -10,11 +10,12 @@ export default class Arbitre {
   private dasher: number;
   private touched: number;
   private world: p2.World;
-  private scene : BABYLON.Scene;
+  private scene: BABYLON.Scene;
   private spriteManagerPlayer: BABYLON.SpriteManager;
   private players: Player[];
 
   private animationsPlayers;
+
   constructor() {
     this.players = [];
     const cosmoAnimation = {
@@ -55,7 +56,7 @@ export default class Arbitre {
 
   static getInstance() {
     if (!Arbitre.instance) {
-        Arbitre.instance = new Arbitre();
+      Arbitre.instance = new Arbitre();
     }
     return Arbitre.instance;
   }
@@ -68,12 +69,12 @@ export default class Arbitre {
     return KeyGenerator.getInstance();
   }
 
-  public setTimerKeys(timerKeys:number) {
+  public setTimerKeys(timerKeys: number) {
     this.timerKeys = timerKeys;
     return this;
   }
 
-  public setKeys(keys:Key[]) {
+  public setKeys(keys: Key[]) {
     this.getKeyGenerator().addKeys(keys);
     return this;
   }
@@ -85,6 +86,7 @@ export default class Arbitre {
 
   public generateKeys() {
     this.getKeyGenerator().generate();
+    return this;
   }
 
   public regenerate() {
@@ -95,10 +97,10 @@ export default class Arbitre {
     }, this.timerKeys);
   }
 
-  public parityDash(dasher:number, touched:number) {
-      const rand = this.getKeyGenerator().getRandomInt(0, 2);
-      this.dasher = (rand == 0 ? dasher : touched);
-      this.touched = (rand == 0 ? touched : dasher);
+  public parityDash(dasher: number, touched: number) {
+    const rand = this.getKeyGenerator().getRandomInt(0, 2);
+    this.dasher = (rand == 0 ? dasher : touched);
+    this.touched = (rand == 0 ? touched : dasher);
   }
 
   public getDasher() {
@@ -113,14 +115,14 @@ export default class Arbitre {
     return this.players;
   }
 
-  public setScene(scene:BABYLON.Scene) {
-    const playersPath = "../assets/Sprites/cosm.png";
+  public setScene(scene: BABYLON.Scene) {
+    const playersPath = '../assets/Sprites/cosm.png';
     this.scene = scene;
-    this.spriteManagerPlayer = new BABYLON.SpriteManager("pm", playersPath, 3, 80, this.scene);
+    this.spriteManagerPlayer = new BABYLON.SpriteManager('pm', playersPath, 3, 80, this.scene);
     this.players = [];
   }
 
-  public createPlayer(name:string, position:number) {
+  public createPlayer(name: string, position: number) {
     const player = new Player(name, this.scene, this.animationsPlayers.cosmo, this.spriteManagerPlayer);
     player.body.position[0] += position;
     this.world.addBody(player.body);
