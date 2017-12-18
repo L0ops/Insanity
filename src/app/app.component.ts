817930334bf05919920ea6fc6e98408faf5f9d3f
Armand Goaziou <armand@frequence-running.com>
2017-12-18 11:01:10 +0100

    ajout background music autoloop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -53,6 +53,9 @@ export class AppComponent implements AfterViewInit {
     scene.actionManager = new BABYLON.ActionManager(scene);
     Environment.getInstance().setScene(scene).createBackgroundPlan();
 
+    // background music
+    let bgMusic = new BABYLON.Sound("bgMusic", "../assets/Music/bgmusic.mp3", scene, null, { loop: true, autoplay: true });
+
     // `const light =` is useless because we don't reuse it later
     const light = new BABYLON.PointLight('Point', new BABYLON.Vector3(5, 10, 5), scene);
     const freeCamera = new BABYLON.FreeCamera('FreeCamera', new BABYLON.Vector3(0, 2, -17), scene);
